ea14b875ac01bcc4957886115c482d950958fd17
ddkurnia <kurnia200688@gmail.com>
2026-02-27 11:08:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -222,7 +222,7 @@ JALANKAN SERVER
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 
   console.log("=================================");
   console.log("KEDAI MEDIA BOT AKTIF");
