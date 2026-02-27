e559f56c5d304a055c3abb52ab33abc215e93b35
ddkurnia <kurnia200688@gmail.com>
2026-02-27 11:59:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require("express");
 
 const app = express();
 
+// ROOT
 app.get("/", (req, res) => {
   res.status(200).send("BOT KEDAI MEDIA AKTIF");
 });
 
+// WEBHOOK
 app.get("/webhook", (req, res) => {
   res.status(200).send("WEBHOOK AKTIF");
 });
 
-const PORT = process.env.PORT;
+// PORT FINAL FIX
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("SERVER JALAN DI PORT:", PORT);
+  console.log("=================================");
+  console.log("SERVER HIDUP");
+  console.log("PORT:", PORT);
+  console.log("=================================");
 });
