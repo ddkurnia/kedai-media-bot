c6abcea986196b6d0dd632f4c8f69146cde87c24
ddkurnia <kurnia200688@gmail.com>
2026-03-14 21:17:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require("express");
 
 const app = express();
 
+/* ROUTE TEST */
+
 app.get("/", (req, res) => {
-  res.send("KEDAI MEDIA BOT RUNNING");
+  res.send("SERVER HIDUP");
 });
 
-app.get("/health", (req, res) => {
-  res.send("OK");
-});
-
-/* START SERVER */
+/* PORT RAILWAY */
 
 const PORT = process.env.PORT || 3000;
 
+/* START SERVER */
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log("================================");
-  console.log("SERVER BERJALAN");
+  console.log("SERVER RUNNING");
   console.log("PORT:", PORT);
   console.log("================================");
 });
