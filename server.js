9c5018643ff93f5e8cad7e6136867a724bf775c2
ddkurnia <kurnia200688@gmail.com>
2026-03-14 21:11:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -177,10 +177,11 @@ ADMIN_NUMBER,
 }
 
 /* SERVER */
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-
-console.log("Server running on port", PORT);
-
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("================================");
+  console.log("KEDAI MEDIA SALES AI PRO AKTIF");
+  console.log("PORT:", PORT);
+  console.log("================================");
 });
